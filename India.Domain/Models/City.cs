06a480a5c4927c9b9d84baeac466fc4bdaae1386
Pratik Chakraborty <pratikchakraborty2020@gmail.com>
2024-12-24 20:12:32 +0530

    Add connection string and add state, city domain models

---
+++ India.Domain/Models/City.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace India.Domain.Models
+{
+    public class City
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public BigInteger Population { get; set; }
+        public Guid StateId { get; set; }
+
+        //navigation properties
+        public State State { get; set; }
+    }
+}
---
+++ India.Domain/Models/State.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace India.Domain.Models
+{
+    public class State
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public BigInteger Population { get; set; }
+        public ICollection<City> Cities { get; set; }
+
+        public Guid CapitalCityId { get; set; }
+        // Navigation Properties
+        public City CapitalCity { get; set; }
+    }
+}
